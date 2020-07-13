4189aea5130db4bae5e65855bce06ca665b5f7bf
Andrew <andrewtanjaya21@gmail.com>
2020-07-13 18:26:35 +0700

    test

--- server.go
+++ server.go
@@ -30,7 +30,7 @@ func main() {
 	// Add CORS middleware around every request
 	// See https://github.com/rs/cors for full option listing
 	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowedOrigins:   []string{"http://localhost:4200"},
 		AllowCredentials: true,
 		Debug:            true,
 	}).Handler)
