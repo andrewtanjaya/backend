57676e27e3438726a7f1a3683641ff180fa9592a
Andrew <andrewtanjaya21@gmail.com>
2020-07-14 10:21:56 +0700

    ini baru bisa

--- server.go
+++ server.go
@@ -15,24 +15,24 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-const defaultPort = "6000"
+const defaultPort = "8000"
 
 func main() {
 	port := os.Getenv("PORT")
 
-	//pgDB := pg.Connect(&pg.Options{
-	//	Addr:     ":5432",
-	//	User:     "oadxreyqmffgnh",
-	//	Password: "1bd75bcd83cf9e171d63dc62dc19be625150e7541b0dc2f2b4ff8fe56706fa69",
-	//	Database: "d7sbv5fgdtfjdr",
-	//})
+	pgDB := pg.Connect(&pg.Options{
+		Addr:     ":5432",
+		User:     "postgres",
+		Password: "131112awt",
+		Database: "Soutube",
+	})
 
-	opt, err := pg.ParseURL("postgres://oadxreyqmffgnh:1bd75bcd83cf9e171d63dc62dc19be625150e7541b0dc2f2b4ff8fe56706fa69@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/d7sbv5fgdtfjdr?sslmode=require")
-	if err != nil {
-		panic(err)
-	}
-
-	pgDB := pg.Connect(opt)
+	//opt, err := pg.ParseURL("postgres://oadxreyqmffgnh:1bd75bcd83cf9e171d63dc62dc19be625150e7541b0dc2f2b4ff8fe56706fa69@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/d7sbv5fgdtfjdr?sslmode=require")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//pgDB := pg.Connect(opt)
 	pgDB.AddQueryHook(postgres.DBLogger{})
 
 	defer pgDB.Close()
@@ -41,7 +41,7 @@ func main() {
 	// Add CORS middleware around every request
 	// See https://github.com/rs/cors for full option listing
 	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:4200/"},
+		AllowedOrigins:   []string{"http://localhost:4200"},
 		AllowCredentials: true,
 		Debug:            true,
 	}).Handler)
